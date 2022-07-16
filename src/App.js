bff1fe78fdb58e8d481e15bd6b3394428130837c
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-16 09:53:03 -0700

    Create card and cards states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+
+  const [card, changeCard] = useState({ id: null, image: null, wasPicked: null });
+  const [cards, changeCards] = useState([]);
+
   return (
     <div className="App">
       
