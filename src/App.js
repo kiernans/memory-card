495218a5b0f93bcda3090ee6ab6a867992e563c9
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-20 15:19:44 -0700

    Create Header and add to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Card from './components/Card';
+import Header from "./components/Header";
+import './styles/App.css'
 
 const App = () => {
 
   return (
     <div className="App">
+      <Header />
       <Card />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Header.css'
+
+const Header = () => {
+
+    return (
+        <div className="Header">
+            <h1>Final Fantasy Memory Game</h1>
+        </div>
+    );
+};
+
+export default Header;
