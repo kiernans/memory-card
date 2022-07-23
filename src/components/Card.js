995dad02cf39f1a5ee43aaffaf0d31b1bf1c9a4e
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-22 19:00:03 -0700

    Create function to reset boolean that shows if card was picked

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import uniqid from 'uniqid';
 import DisplayCards from "./DisplayCards";
 
-const Card = ({ score, bestScore, incrementScore, updateBestScore }) => {
+const Card = ({ score, bestScore, incrementScore, updateBestScore, resetScore }) => {
 
 	const [cards, setCards] = useState([]);
 	const [card, setCard] = useState({ id: uniqid(), image: require(`../images/${cards.length + 1}.png`), wasPicked: false });
@@ -23,14 +23,27 @@ const Card = ({ score, bestScore, incrementScore, updateBestScore }) => {
 		return cards.sort((a,b) => 0.5 - Math.random());
 	};
 
+	const resetWasPicked = () => {
+		const newCards = cards.map(card => {
+			return { ...card, wasPicked: false }
+		});
+		console.log(`newCards: `, newCards);
+		setCards(newCards);
+	};
+
 	const handleOnClick = (e) => {
 		const id = e.target.id ? e.target.id : e.target.parentElement.id;
-		
-		if(cards.filter(card => card.id === id).wasPicked) {
-			updateBestScore();
+		const cardChosen = cards.filter(card => card.id === id)[0].wasPicked;
+
+		if(cardChosen) {
+			if(score > bestScore) updateBestScore();
+			console.log(`Before: `, cards);
+			resetWasPicked();
+			console.log(`After: `, cards);
+			resetScore();
 		} else incrementScore();
 		
-		const newState = cards.map(card => {
+		const newCards = cards.map(card => {
 			if (card.id === id) {
 				return { ...card, wasPicked: true };
 			}
@@ -38,7 +51,7 @@ const Card = ({ score, bestScore, incrementScore, updateBestScore }) => {
 			return card;
 		});
 
-		setCards(shuffleCards(newState));
+		setCards(shuffleCards(newCards));
 	};
 
 	return (
